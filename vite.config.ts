9c0209f9bc633a70ab2835af6df5972c0d9a59e6
minhtris2005 <phamnguyenminhtri249@gmail.com>
2025-11-20 20:40:20 -0800

    .

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/HITEK_CLONE/",
   server: {
     host: "::",
     port: 8080,
